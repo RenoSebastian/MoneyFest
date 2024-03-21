05fa71109ad856af9b93c001ef620844e003a693
syira <syira.khoerunisa.tif22@polban.ac.id>
2024-03-21 12:41:43 +0700

    register

--- money_fest_frontend/lib/register.dart
+++ money_fest_frontend/lib/register.dart
@@ -14,19 +14,39 @@ class Register extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Ini Halaman Register',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
+              // Tambahkan gambar di atas teks
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Image.asset(
+                  'MoneyFestLogo.png', // Ubah dengan path gambar Anda
+                  width: 150, // Sesuaikan lebar gambar
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // Navigasi ke halaman dashboard
-                  Navigator.pushReplacementNamed(context, '/dashboard');
-                },
-                child: const Text('Next'),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    'images/Base2.png',
+                    height: 400,
+                  ),
+                  const Positioned(
+                    top: 25,
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigasi ke halaman dashboard
+                      Navigator.pushReplacementNamed(context, '/dashboard');
+                    },
+                    child: const Text('Next'),
+                  ),
+                ],
               ),
             ],
           ),

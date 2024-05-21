032e76ed5047950a0aad0f84de7ac2d89b88362c
syira <syira.khoerunisa.tif22@polban.ac.id>
2024-05-21 08:51:13 +0700

    Subkategori

--- MoneyFest-Backend/app/Models/SubKategoriModel.php
+++ MoneyFest-Backend/app/Models/SubKategoriModel.php
@@ -27,6 +27,13 @@ class SubKategoriModel extends Model
             // Hitung total uang dari subkategori terkait dan update jumlah di kategori
             $kategori->jumlah = $kategori->subKategoris()->sum('uang');
             $kategori->save();
+
+            // Update balance
+            $balance = BalanceModel::where('user_id', $subKategori->user_id)->first();
+            if ($balance) {
+                $balance->balance -= $subKategori->uang;
+                $balance->save();
+            }
         });
     }
 
@@ -40,39 +47,3 @@ class SubKategoriModel extends Model
         return $this->belongsTo(User::class);
     }
 }
-
-// <?php
-
-// namespace App\Models;
-
-// use Illuminate\Database\Eloquent\Model;
-
-// class SubKategoriModel extends Model
-// {
-//     protected $table = 'SubKategori';
-
-//     protected $fillable = [
-//         'NamaSub',
-//         'uang',
-//     ];
-
-//     protected static function boot()
-// {
-//     parent::boot();
-
-
-//     static::saved(function ($subKategori) {
-//         // Ambil kategori terkait
-//         $kategori = $subKategori->kategori;
-
-//         // Hitung total uang dari subkategori terkait dan update jumlah di kategori
-//         $kategori->jumlah = $kategori->subKategoris()->sum('uang');
-//         $kategori->save();
-//     });
-// }
-
-//     public function kategori()
-//     {
-//         return $this->belongsTo(KategoriModel::class, 'kategori_id');
-//     }
-// }
